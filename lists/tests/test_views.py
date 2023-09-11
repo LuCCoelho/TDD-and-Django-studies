b3fed6f71626b23808c3773bbbc3ff36c7df7a58
luisaccoelho <luisacoelho@ufba.br>
2023-09-11 11:40:47 -0300

    refactor: use new form in home_page, simplify tests. NB breaks stuff

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from lists.models import Item, List
 from django.utils.html import escape
+from lists.forms import ItemForm
 
 # Create your tests here.
 
@@ -9,6 +10,11 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
+
     
 class ListViewTest(TestCase):
 
@@ -51,7 +57,6 @@ class ListViewTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
-
 
     def test_POST_redirects_to_list_view(self):
         other_list = List.objects.create()
